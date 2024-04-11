df7f7eaa947a65be2652a91a17c2929c6621a3e8
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 02:18:19 +0000

    add Lesson3_Homework

---
+++ Lesson3/Homework/Program.cs
@@ -0,0 +1,56 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i;
+
+void FillArray(int[] arr)
+{
+    i = 0;
+    while (i < n)
+    {
+        arr[i] = i + 1;
+        i++;
+    }
+}
+
+void PrintArray(int[] arr) 
+{
+    i = 0;
+    while (i < n)
+    {
+        Console.Write($"{arr[i]}");
+        i++;
+    }
+}
+
+int GetSumOfElements(int[] arr) 
+{
+    i = 0;
+    int sum = 0;
+    while (i < n)
+    {
+        sum = sum + arr[i];
+        i++;
+    }
+    return sum;
+}
+int GetProductOfElements(int[] arr)
+{
+    i =0;
+    int product = 1;
+    while (i < n)
+    {
+        product = product * arr[i];
+        i++;
+    }
+    return product;
+}
+
+FillArray(arr);
+PrintArray(arr);
+
+int sum = GetSumOfElements(arr);
+int product = GetProductOfElements(arr);
+
+Console.WriteLine(" ");
+Console.WriteLine(sum);
+Console.WriteLine(product);
