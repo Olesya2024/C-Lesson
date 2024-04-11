78378e0141446b29c6fa3a8bc08f25eb9df904a4
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 02:37:44 +0000

    add Lesson4_Task2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿// • Назовём число «интересным» если его сумма
+// цифр чётная
+// • Создать двумерный массив, состоящий из
+// целых чисел. Вывести на экран «интересные»
+// элементы массива
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(10, 1000);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (SumOfNum(matrix[i, j]) % 2 == 0)
+            {
+                Console.Write($"{matrix[i, j]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumOfNum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int[,] matrix = CreateMatrix(4,5);
+ShowMatrix(matrix);
