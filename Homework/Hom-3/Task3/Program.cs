d8de2188d22e734b3db44ba8e01c49823cd7dd7d
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 17:18:46 +0000

    add Hom-3_Task3

--- Homework/Hom-3/Task3/Program.cs
+++ Homework/Hom-3/Task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+bool PalindromicCheckBool(string input)
+{
+    for (int i = 0; i < input.Length / 2; i++)
+    {
+        if (input[i] != input[input.Length - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void ConclusionYesOrNot(bool palindrom)
+{
+    if (palindrom == true)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+Console.WriteLine("Введите строку");
+string input = Console.ReadLine();
+
+bool palindrom = PalindromicCheckBool(input);
+ConclusionYesOrNot(palindrom);
