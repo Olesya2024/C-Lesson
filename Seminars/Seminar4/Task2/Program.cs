423e4f67a3fe4070d082ac3f8786ae36be74ab6e
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 16:19:26 +0000

    add Seminar4_Task2

---
+++ Seminars/Seminar4/Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример:
+// [1 5 11 21 81 4 0 91 2 3] => 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+int CountResultEndNumdivisorNum(int[] array, int EndNum, int divisorNum)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % EndNum == 0 && array[i] % divisorNum == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Задайте размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRndInt(n, 1, 100);
+PrintArray(arr);
+
+int countResultEndNumdivisorNum = CountResultEndNumdivisorNum(arr, 1, 7);
+Console.Write($" => {countResultEndNumdivisorNum}");
