1379729bc94d7913b89ca2a008d4d98968b372da
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 17:17:56 +0000

    add Hom-3_Task4

---
+++ Homework/Hom-3/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+// “Hello my world” => “world my Hello”
+
+void SupplyReversal(string hello)
+{
+    string res = string.Empty;
+    string[] helloSplit = hello.Split();
+
+    for (int i = helloSplit.Length - 1; i >= 0; i--)
+    {
+        res += helloSplit[i] + " ";
+    }
+
+    Console.WriteLine(res.TrimEnd());
+}
+
+string hello = "Hello my world";
+
+SupplyReversal(hello);
