021663461d1742a87224446420c3051c7dabdf82
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 15:52:51 +0000

    add Seminar1_Task3

---
+++ Seminars/Seminar1/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int result = number % 10 + number / 100;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
