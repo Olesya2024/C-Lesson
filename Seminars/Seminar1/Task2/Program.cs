d890c332c104f648a87820232157e96a553403c5
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 15:47:35 +0000

    add Seminar1_Task2

---
+++ Seminars/Seminar1/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// /*Задание 2. 
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите целое число N:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = -n; i <= n; i++)
+        {
+            Console.Write(i);
+            if (i != n)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
