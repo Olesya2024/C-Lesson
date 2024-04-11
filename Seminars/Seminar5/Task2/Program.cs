e2fdf3ee925bf4a4ae8f4f7202ceed9b50390a78
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 16:24:51 +0000

    add Seminar5_Task2

---
+++ Seminars/Seminar5/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+// Пример
+// 2 3 4 3
+//  4 3 4 1 => 2 + 3 + 5 = 10
+//  2 9 5 4
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+int SumElementsOfMainDiagonalOfMAtrix(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+
+}
+
+int[,] arr2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(arr2d);
+Console.WriteLine();
+int sum = SumElementsOfMainDiagonalOfMAtrix(arr2d);
+Console.WriteLine(sum);
