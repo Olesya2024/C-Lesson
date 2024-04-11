fe15f0817252818861d53b3d234772566659e09c
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 17:06:24 +0000

    add Hom-2_Task2

---
+++ Homework/Hom-2/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 2: Задайте массив заполненный случайными
+// трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в
+// массиве.
+// [344 452 341 125] => 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+int FindEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void CallingFunctions() 
+{
+    int[] arr = CreateArrayRndInt(4, 100, 1000);
+    PrintArray(arr);
+
+    int findEven = FindEven(arr);
+    Console.Write($" => {findEven}");
+}
+
+CallingFunctions();
