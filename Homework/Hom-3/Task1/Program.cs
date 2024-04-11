7583ba21e2a31dd317869a7a4bc724430e2f6057
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-04-11 17:11:15 +0000

    add Hom-3_Task1

---
+++ Homework/Hom-3/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+// a b c
+// d e f => “abcdef” 
+
+// string CharsArrayToString(char[,] chars)
+// {
+//     string strResult = string.Empty;
+
+//     for (int i = 0; i < chars.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < chars.GetLength(1); j++)
+//         {
+//             strResult += chars[i, j];
+//         }
+//     }
+
+//     return strResult;
+// }
+
+// char[,] chars = { {'a', 'b', 'c'}, {'d', 'e', 'f'} };
+
+// string strResult = CharsArrayToString(chars);
+
+// Console.WriteLine(strResult);
+
+string CharsArrayToString(char[,] chars)
+{
+
+string strResult = string.Empty;
+
+for (int i = 0; i < chars.GetLength(0); i++)
+{
+    for (int j = 0; j < chars.GetLength(1); j++)
+    {
+        strResult += chars[i, j];
+    }
+}
+return strResult;
+}
+char[,] chars = { {'a', 'b', 'c'}, {'d', 'e', 'f'} };
+string strResult = CharsArrayToString(chars);
+Console.WriteLine(strResult);
